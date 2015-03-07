8e8c7eb665be753b9ca632d1fea48d0df208753b
nata.sesteves@gmail.com <nata.sesteves@gmail.com@86df9113-bbcd-e8c3-8fdb-b5f5865647ed>
2015-03-07 06:08:07 +0000

    Primeiros progressos

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	public string scene; 
+	
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnMouseOver() 
+	{
+		renderer.sprite = mouseOverSprite;
+	}
+
+	void OnMouseExit()
+	{
+
+	}
+
+	void OnMouseDown()
+	{
+
+	}
+
+	void OnMouseUp()
+	{
+		Application.LoadLevel(scene);
+
+	}
+
+}
